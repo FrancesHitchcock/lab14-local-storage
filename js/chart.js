01dd7ca25d951767303aa975e076330c390abd6c
Frances Hitchcock <fran.h.hitchcock@gmail.com>
2023-06-29 15:38:57 +0100

    addition of local storage and retrieval functionality and chart display

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-let canvasElem = document.getElementById('chart')
+let canvasElem = document.getElementById("chart");
 
 /* TODO:
  * - Instantiate a new AppState
@@ -10,7 +10,81 @@ let canvasElem = document.getElementById('chart')
  * - Call chart.js with the configuration and the canvasElem
  *
  */
+
+let chartState = new AppState();
+
+chartState.loadItems();
+
+const allProductsToDisplay = chartState.allProducts;
+
 function renderChart() {
+  const chartArea = document.getElementById("chart");
+  const productViews = [];
+  const productClicks = [];
+  const allProductNames = [
+    "bag",
+    "banana",
+    "bathroom",
+    "boots",
+    "breakfast",
+    "bubblegum",
+    "chair",
+    "cthulhu",
+    "dog-duck",
+    "dragon",
+    "pen",
+    "pet-sweep",
+    "scissors",
+    "shark",
+    "sweep",
+    "tauntaun",
+    "unicorn",
+    "water-can",
+    "wine-glass",
+  ];
+
+  for (let i = 0; i < allProductsToDisplay.length; i++) {
+    productViews.push(allProductsToDisplay[i].timesShown);
+    productClicks.push(allProductsToDisplay[i].timesClicked);
+  }
+
+  //   console.log(productViews, productClicks, allProductNames);
+
+  const barChartData = {
+    labels: allProductNames,
+    datasets: [
+      {
+        label: "Views",
+        data: productViews,
+        backgroundColor: ["yellow"],
+        borderColor: ["sienna"],
+        borderWidth: 1,
+      },
+      {
+        label: "Clicks",
+        data: productClicks,
+        backgroundColor: ["orange"],
+        borderColor: ["sienna"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const barChart = new Chart(chartArea, {
+    type: "bar",
+    data: barChartData,
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            stepSize: 1,
+          },
+        },
+      },
+    },
+  });
+
+  localStorage.clear();
 }
 
 renderChart();
